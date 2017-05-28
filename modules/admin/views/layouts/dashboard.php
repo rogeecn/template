cda82104e279d68e4e0e842b70a8f453ea8f9d2d
yanghao <yanghao@b.360.cn>
2017-05-28 19:50:38 +0800

    add long text support add spider api

--- modules/admin/views/layouts/dashboard.php
+++ modules/admin/views/layouts/dashboard.php
@@ -14,7 +14,7 @@
     <title>后台</title>
     <?php $this->head() ?>
     <style>
-        .layui-body .layui-tab-item{
+        .layui-body .layui-tab-item {
             height: 99.5%;
         }
     </style>
@@ -23,15 +23,27 @@
 <?php $this->beginBody() ?>
 <div class="layui-layout layui-layout-admin">
     <div class="layui-header" style="text-align: right;">
-        <?=$this->render("_topnav");?>
+        <a href="/admin" id="logo">やめて</a>
+        <?= \common\extend\Html::ul([
+            \common\extend\Html::a("访问首页", $this->setting("site.url"), ['target' => "_blank"]),
+            \common\extend\Html::a("清理缓存", '/admin/clear-cache'),
+            \common\extend\Html::a("退出", '/admin/logout'),
+        ], [
+            'class'       => 'layui-nav',
+            'encode'      => FALSE,
+            'itemOptions' => [
+                'class' => 'layui-nav-item',
+            ],
+        ]) ?>
     </div>
     <div class="layui-side layui-bg-black">
         <div class="layui-side-scroll">
-            <?=$this->render("_sidenav");?>
+            <?= $this->render("_sidenav"); ?>
         </div>
     </div>
     <div class="layui-body" style="bottom: 0px;">
-        <div class="layui-tab layui-tab-brief main-tab-container" lay-filter="tab-container"  lay-allowclose="true" style="margin: 0">
+        <div class="layui-tab layui-tab-brief main-tab-container" lay-filter="tab-container" lay-allowclose="true"
+             style="margin: 0">
             <ul class="layui-tab-title site-demo-title"></ul>
             <div class="layui-body layui-tab-content" style="padding:0px;left: 0px;top:40px;bottom: 0px"></div>
         </div>
@@ -40,28 +52,28 @@
 <?php $this->endBody() ?>
 <script>
     var element = layui.element();
-    $(function(){
-        $("body").on("click",".link-to-tab a",function(e){
-            console.log("click",$(this))
+    $(function () {
+        $("body").on("click", ".link-to-tab a", function (e) {
+            console.log("click", $(this))
             e.preventDefault();
             e.stopPropagation();
 
             var link = $(this).attr("href");
-            if (link.length == 0 || link == "#"){
+            if (link.length == 0 || link == "#") {
                 return false;
             }
 
-            if ($(".main-tab-container .layui-tab-title li[lay-id='"+link+"']").length > 0){
+            if ($(".main-tab-container .layui-tab-title li[lay-id='" + link + "']").length > 0) {
                 element.tabChange('tab-container', link);
 
-                $(".layui-body .layui-show").find("iframe").attr("src",link);
+                $(".layui-body .layui-show").find("iframe").attr("src", link);
                 return false;
             }
 
             var _iframeTpl = '<iframe src="_HREF_" frameborder="0" width="100%" height="100%"></iframe>';
             element.tabAdd('tab-container', {
                 title: $(this).text(),
-                content: _iframeTpl.replace("_HREF_",link),
+                content: _iframeTpl.replace("_HREF_", link),
                 id: link,
             });
             element.tabChange('tab-container', link);
