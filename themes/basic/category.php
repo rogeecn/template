9f0c5ee1a3a690fcca2d5b7780b4fdc8990de927
rogee <rogee@foxmail.com>
2017-06-03 09:59:37 +0800

    add tabshow javascript

--- themes/basic/category.php
+++ themes/basic/category.php
@@ -3,12 +3,21 @@
 /** @var $this \common\extend\View */
 /** @var $categoryChildren \common\models\Category[] */
 
+$categoryList = array_chunk($categoryChildren, 2);
 ?>
 <?= \widgets\Breadcrumbs::widget(['categoryID' => $categoryID]) ?>
 
-<div class="content">
-    <?php foreach ($categoryChildren as $categoryChild): ?>
-        <?= \widgets\CategoryBox::widget(['categoryAlias' => $categoryChild->alias]) ?>
-    <?php endforeach; ?>
-</div>
+    <div class="content">
+        <?php foreach ($categoryList as $categoryChildren): ?>
+            <div class="row">
+                <div class="col col-left">
+                    <?= \widgets\CategoryBox::widget(['categoryAlias' => $categoryChildren[0]->alias]) ?>
+                </div>
+                <div class="col col-right">
+                    <?= \widgets\CategoryBox::widget(['categoryAlias' => $categoryChildren[1]->alias]) ?>
+                </div>
+            </div>
+        <?php endforeach; ?>
+    </div>
+
 <?= $this->render("sider") ?>
