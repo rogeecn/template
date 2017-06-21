7ab5544d07008b2e7a35dde2816049e2b1534e09
yanghao <yanghao@b.360.cn>
2017-06-21 12:16:13 +0800

    update create

---
+++ console/controllers/ClearEmptyContentController.php
@@ -0,0 +1,71 @@
+<?php
+namespace console\controllers;
+
+
+use common\base\Field;
+use common\models\Article;
+use common\models\ArticleField;
+use common\models\Tag;
+use common\models\TagArticle;
+use common\util\Logger;
+use console\base\ConsoleController;
+use yii\base\Exception;
+use yii\helpers\ArrayHelper;
+
+class ClearEmptyContentController extends ConsoleController
+{
+    /** @var  \Monolog\Logger */
+    private static $_logger;
+
+    public function beforeAction($action)
+    {
+        self::$_logger = Logger::instance($this->uniqueId);
+
+        return parent::beforeAction($action);
+    }
+
+    public function actionIndex()
+    {
+        $data   = \Yii::$app->getDb()->createCommand("select id from field_content_data where content = ''")->queryAll();
+        $idList = ArrayHelper::getColumn($data, "id");
+
+        foreach ($idList as $id) {
+            $articleModel = Article::findOne($id);
+            if (!$articleModel) {
+                self::$_logger->info(sprintf("%d not exist...", $id));
+                continue;
+            }
+
+            $typeFields = ArticleField::getTypeFieldList($articleModel->type);
+            $trans      = \Yii::$app->getDb()->beginTransaction();
+            try {
+                if (!$articleModel->delete()) {
+                    throw new Exception(json_encode($articleModel->getErrors()));
+                }
+
+                foreach ($typeFields as $field) {
+                    $field['class']::field([
+                        'action' => Field::ACTION_DELETE,
+                        'config' => $field,
+                        'dataID' => $id,
+                    ]);
+                }
+
+                $articleTags = TagArticle::getArticleTags($id);
+                foreach ($articleTags as $articleTag) {
+                    Tag::descTagReferenceCountByID($articleTag['id']);
+                }
+
+                TagArticle::removeArticle($id);
+
+
+                self::$_logger->info(sprintf("%d remove successful...", $id));
+                $trans->commit();
+            } catch (\Exception $e) {
+                $trans->rollBack();
+                self::$_logger->info(sprintf("%d remove failed...", $id));
+            }
+
+        }
+    }
+}
